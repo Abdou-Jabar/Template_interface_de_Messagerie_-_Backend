331e230db5bb9dd0d8e711b7ea130abd2782fef1
Abdou-Jabar Mikailou <151407343+Abdou-Jabar@users.noreply.github.com>
2024-10-19 18:38:30 +0000

    Update messagerie.php

--- messagerie.php
+++ messagerie.php
@@ -1,20 +1,35 @@
 <?php
     include 'db/connect.php';
     session_start();
-    $id_utilisateur = $_SESSION['id_utilisateur'];
+    
+    //Verifions si un message a été envoyé
     if(isset($_POST['message'])){
+        //Récupération du message ne faisant pas confiance à l'utilisateur et pour éviter des erreurs (utilisation de htmlentites())
         $message = htmlentities($_POST['message']);
+
+        //Récupération de la date au moment de l'envoi du message
         $date = date('H:i:s');        
+
+        //Requête pour Récuperer le dernier message
         $sql_dernier_message = "SELECT * FROM message
                                 ORDER BY id_message DESC
                                 LIMIT 1";
+                                
+        //On vérifie si un message à été récupérer
         if($conn->query($sql_dernier_message)->num_rows > 0){
+
+            /**
+             * Récupération du dernier message en tableau
+             * Si ce message est différent du message qui vient d'être envoyer, on l'insert dans la base
+            **/
+
             $row = $conn->query($sql_dernier_message)->fetch_assoc();
             if($row['message'] != $_POST['message']){
                 $sql = "insert into message (`id_utilisateur`, `message`, `heure`) values('$id_utilisateur','$message','$date')";
                 $result = $conn->query($sql);
             }
         }
+        //Toutefois si la base de donnée de message est vide, on insert le message sans vérification
         else {
             $sql = "insert into message (`id_utilisateur`, `message`, `heure`) values('$id_utilisateur','$message','$date')";
             $result = $conn->query($sql);
@@ -46,11 +61,13 @@
         </form>
     </div>
     <script>
+        //Script qui se charge du défilement automatique vers des messages pour afficher le dernier message envoyer
         window.onload = function() {
             var messageDiv = document.getElementById("messages");
             messageDiv.scrollTop = messageDiv.scrollHeight;
         };
 
+        //Script qui charge à chaque demi seconde le fichier php qui affiche les messages, de ce fait on aura plus à actualiser la page pour afficher les nouveaux messages reçus
         setInterval('load_message()', 500);
         function load_message() {
             $('#messages').load('db/read.php')
