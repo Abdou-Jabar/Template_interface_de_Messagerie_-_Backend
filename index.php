aa539506b1aa8bc3e6321d2633f291ee19fc8642
Abdou-Jabar Mikailou <151407343+Abdou-Jabar@users.noreply.github.com>
2024-10-19 18:38:03 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,33 +1,55 @@
 <?php
+    //Importation du fichier de connexion à la base de donnée
     require 'db/connect.php';
     
+    //Vérification: on verifie si un pseudo est défini
     if(isset($_POST['pseudo'])){
+
         $pseudo = $_POST['pseudo'];
-        $sql = "select * from utilisateur where pseudo = '$pseudo'";
+
+        //Requête pour vérifier si l'identifiant de l'utilisateur est déjà dans notre base de donnée
+        $sql = "SELECT id_utilisateur from utilisateur where pseudo = '$pseudo'";
         $result = $conn->query($sql);
+
+        /**
+         * La bloc de condition <if> s'éxécute si l'identifiant est déjà présent dans la base
+         * 
+         * Au cas contraire <else> s'éxécutera
+         */
+
         if($result->num_rows > 0){
+
+            //Redirection vers l'interface de message
             header("Location: messagerie.php");
             session_start();
-            $sql = "SELECT id_utilisateur FROM utilisateur WHERE pseudo = '$pseudo'";
-            $result = $conn->query($sql);
-            if ($result->num_rows > 0) {
-                $row = $result->fetch_assoc();
-                $id_utilisateur = $row['id_utilisateur'];
-            }
+            
+            /*
+             * Récupération des données de la base sous forme de tableau
+             * Récupération de l'identifiant de l'utilisateur dans une variable
+             * Création d'une session avec la clé id_utilisateur
+             */
+            
+            $row = $result->fetch_assoc();
+            $id_utilisateur = $row['id_utilisateur'];
             $_SESSION['id_utilisateur'] = $id_utilisateur;
             exit();
         }
         else {
+            //Insertion de l'identifiant de l'utilisateur dans la base de donnée
             $sqlinsert = "insert into utilisateur (`pseudo`) values ('$pseudo')";
             if($conn->query($sqlinsert) === TRUE){
+                //Redirection vers l'interface de message
                 header("Location: messagerie.php");
                 session_start();
-                $sql = "SELECT id_utilisateur FROM utilisateur WHERE pseudo = '$pseudo'";
-                $result = $conn->query($sql);
-                if ($result->num_rows > 0) {
-                    $row = $result->fetch_assoc();
-                    $id_utilisateur = $row['id_utilisateur'];
-                }
+
+                /*
+                 * Récupération des données de la base sous forme de tableau
+                 * Récupération de l'identifiant de l'utilisateur dans une variable
+                 * Création d'une session avec la clé id_utilisateur
+                 */
+
+                $row = $result->fetch_assoc();
+                $id_utilisateur = $row['id_utilisateur'];
                 $_SESSION['id_utilisateur'] = $id_utilisateur;
                 exit();
             }
