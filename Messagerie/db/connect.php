0d22b34ef7b2af5f23ecfe4d4f693fe406f3d849
Abdou-Jabar Mikailou <151407343+Abdou-Jabar@users.noreply.github.com>
2024-10-19 17:39:08 +0000

    Delete Messagerie directory

--- Messagerie/db/connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "djabar00";
-    $dbname = "message";
-
-    $conn = new mysqli($servername, $username, $password, $dbname);
-?>
